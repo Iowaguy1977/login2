b38382e73fbe720bfe12a2c868111048664475f8
chris kalkas <chris.kalkas@icloud.com>
2025-08-20 15:59:08 -0500

    Add project files.

---
+++ login2/SqlConnectionService .cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+
+namespace login2
+{
+    public class SqlConnectionService: IDisposable
+    {
+        private readonly SqlConnection _connection;
+
+        public SqlConnectionService(IConfiguration configuration)
+        {
+            var connString = configuration.GetConnectionString("DefaultConnection");
+            _connection = new SqlConnection(connString);
+            _connection.Open();
+        }
+        public SqlCommand CreateCommand(string sql)
+        {
+            return new SqlCommand(sql, _connection);
+        }
+
+        public void Dispose()
+        {
+            _connection?.Dispose();
+        }
+    }
+}
+
