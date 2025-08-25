0fd6adf49cadebe477184fc33ea289e6a87f8e32
chris kalkas <chris.kalkas@icloud.com>
2025-08-25 12:16:47 -0500

    working Project

---
+++ login2/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿namespace login2
+{
+    public class LoginModel
+    {
+       
+        
+            public int Id { get; set; }
+            public string Username { get; set; } = "";
+            public string Password { get; set; } = "";
+
+
+        
+    }
+}
--- login2/Program.cs
+++ login2/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddScoped<login2.SqlConnectionService>();
 builder.Services.AddScoped<SqlConnectionService>();
 var app = builder.Build();
 
